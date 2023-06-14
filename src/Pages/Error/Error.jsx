72b39fd56a1c2761e127d903b517613ecf9be139
abu-jafor <abujaforwd@yahoo.com>
2023-06-14 20:51:46 +0600

    add home redirect

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 const Error = () => {
   return (
     <div className="text-center mt-20">
       <h1 className="text-4xl font-bold p-4">404 error?</h1>
+      <Link to={"/"}>Home</Link>
     </div>
   );
 };

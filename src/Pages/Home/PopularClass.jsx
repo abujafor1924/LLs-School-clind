56bc1b1fd54830aa612d0da296255402a405edd2
abu-jafor <abujaforwd@yahoo.com>
2023-06-14 19:03:23 +0600

    somes change css

--- src/Pages/Home/PopularClass.jsx
+++ src/Pages/Home/PopularClass.jsx
@@ -15,13 +15,9 @@ const PopularClass = () => {
       <h1 className="text-4xl font-bold">Popular Class</h1>
       <div className="grid md:grid-cols-4 gap-4 mx-auto">
         {data.map((pd) => (
-          <div key={pd._id} className="card w-96 bg-base-100 shadow-xl">
+          <div key={pd._id} className="card w-full bg-base-100 shadow-xl">
             <figure className="px-10 pt-10">
-              <img
-                src={pd?.photo}
-                alt="Shoes"
-                className="rounded-xl h-72 w-96"
-              />
+              <img src={pd?.photo} alt="Shoes" className="rounded-xl h-60 " />
             </figure>
           </div>
         ))}
